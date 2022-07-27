b94e8c655f11db078aa9fbe729704932a2d3fc75
Paulo Reizinho <paulolopesreizinho@gmail.com>
2022-07-27 16:42:41 +0200

    chore:hobbies section to desktop

--- components/_homepage/MyPassions/index.jsx
+++ components/_homepage/MyPassions/index.jsx
@@ -6,6 +6,7 @@ import { Colors } from "../../../assets/variables";
 import { hobbies } from "../../../assets/data";
 import HobbyComponent from "./hobby";
 import Hexagon from "../../../assets/images/homepage/hexagon_big.svg";
+import Polygons from "../../../assets/images/homepage/poligons.svg";
 import Hobby_01 from "../../../assets/images/homepage/hobby_01.png";
 import Hobby_02 from "../../../assets/images/homepage/hobby_02.png";
 import Hobby_03 from "../../../assets/images/homepage/hobby_03.png";
@@ -16,6 +17,7 @@ export default function MyPassions() {
 
   return (
     <SectionContainer>
+      <BackgroundGradient></BackgroundGradient>
       <h1>{t("home:hobbies_title")}</h1>
       <HexagonBig>
         <Image src={Hexagon} alt="hexagon" />
@@ -24,27 +26,35 @@ export default function MyPassions() {
         <div className="hobby hobby__01">
           <Image src={Hobby_01} layout="fixed" alt="" />
           <div className="text">
-            <h1>CODE_&_LEARN</h1>
-            <h2>
-             
-            </h2>
+            <h1>FITNESS_&_GYM</h1>
+            <h2>dsfsdfsd sdfsdfsd dsf sfsdf sdfsdf sdfsdfs</h2>
           </div>
         </div>
         <div className="hobby hobby__02">
           <Image src={Hobby_02} layout="fixed" alt="" />
+          <div className="text">
+            <h1>CODE_&_LEARN</h1>
+            <h2>dsfsdfsd sdfsdfsd dsf sfsdf sdfsdf sdfsdfs</h2>
+          </div>
         </div>
         <div className="hobby hobby__03">
           <Image src={Hobby_03} layout="fixed" alt="" />
+          <div className="text">
+            <h1>HIKING_&_NATURE</h1>
+            <h2>dsfsdfsd sdfsdfsd dsf sfsdf sdfsdf sdfsdfs</h2>
+          </div>
         </div>
         <div className="hobby hobby__04">
-          <Image
-            className="hobby hobby__04"
-            src={Hobby_04}
-            layout="fixed"
-            alt=""
-          />
+          <Image src={Hobby_04} layout="fixed" alt="" />
+          <div className="text">
+            <h1>COOK_&_INVENT</h1>
+            <h2>dsfsdfsd sdfsdfsd dsf sfsdf sdfsdf sdfsdfs</h2>
+          </div>
         </div>
       </HobbyElements>
+      <div className="polygons">
+      <Image src={Polygons} layout="fixed" alt="" />
+      </div>
     </SectionContainer>
   );
 }
@@ -71,6 +81,13 @@ const SectionContainer = styled.div`
       font-size: 3.5rem;
     }
   }
+
+  .polygons {
+    bottom: -90px;
+    right: 0;
+    width: 485px;
+    position: absolute;
+  }
 `;
 
 const HexagonBig = styled.div`
@@ -84,24 +101,51 @@ const HobbyElements = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+  position: relative;
 
   .hobby {
+    z-index: 2;
+    margin-right: auto;
+    cursor: pointer;
+    :hover {
+        .text {
+          opacity: 1;
+          left: 0;
+        }
+      }
     .text {
       z-index: 2;
       position: absolute;
+      top: 50%;
+      left: -50%;
+      transform: translate(5%, -100%);
+      opacity: 0;
+      transition: all 0.4s ease-in-out;
     }
+    
     &__01 {
-      padding-left: 70px;
+      margin-left: 70px;
     }
     &__02 {
-      padding-left: 514px;
+      margin-left: 514px;
       margin-bottom: 200px;
+
     }
     &__03 {
-      padding-left: 514px;
+      margin-left: 514px;
+
     }
     &__04 {
-      padding-left: 70px;
+      margin-left: 70px;
     }
   }
+`;
+
+const BackgroundGradient = styled.div`
+  position: absolute;
+
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(0deg, #0F2027 0%, rgba(15, 32, 39, 0.2) 38.54%, rgba(15, 32, 39, 0) 100%);
 `;
