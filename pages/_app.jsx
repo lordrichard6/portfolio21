e9629c0f951500a81c9374f047bd12eb11d007e5
lordrichard6 <paulolopesreizinho@gmail.com>
2022-08-23 16:46:47 +0200

    chore:package added and configured

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,7 @@
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+
 import "../styles/globals.scss";
 import "../styles/custom.scss";
 import "../styles/hexagon.scss";
@@ -5,6 +9,10 @@ import "../styles/aboutme.scss";
 import "../styles/gallery.scss"
 import "../styles/hobbies.scss"
 
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
