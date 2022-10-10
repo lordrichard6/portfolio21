5647c611a4e741c115336ade98d2e83d26b0b7f7
lordrichard6 <paulolopesreizinho@gmail.com>
2022-10-10 15:52:48 +0200

    chore:added blog boilerplate

--- next.config.js
+++ next.config.js
@@ -4,4 +4,7 @@ const nextTranslate = require("next-translate");
 module.exports = {
   reactStrictMode: false,
   ...nextTranslate(),
+  images: {
+    domains: ['external-content.duckduckgo.com', 'news.stanford.edu', 'miro.medium.com', 'static.wikia.nocookie.net'],
+  },
 }
