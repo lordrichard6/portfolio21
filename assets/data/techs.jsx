34b37cc7089a310b15a618c1baaedff3f915dfaf
lordrichard6 <paulolopesreizinho@gmail.com>
2023-03-02 09:40:15 +0100

    chore:set up the data

---
+++ assets/data/techs.jsx
@@ -0,0 +1,56 @@
+import {
+  SiJavascript,
+  SiReact,
+  SiNextdotjs,
+  SiAngular,
+  SiPython,
+  SiDjango,
+  SiGraphql,
+} from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+
+export const frontTechs = [
+  {
+    tech: "Javascript",
+    icon: <SiJavascript />,
+    text: "In the land of web development, from the fires of Mount Doom, it emerged to become the One Language to rule them all",
+  },
+  {
+    tech: "ReactJS",
+    icon: <SiReact />,
+    text: "Its modular structure allows for a clear and organized approach to programming, enhancing the cognitive fluency of developers.",
+  },
+  {
+    tech: "NextJS",
+    icon: <SiNextdotjs />,
+    text: "The magical unicorn of React frameworks that lets you ride server-side rendering into the sunset.",
+  },
+  {
+    tech: "Angular",
+    icon: <SiAngular />,
+    text: "Because sometimes you just need a framework that's as opinionated as your grandma on Christmas dinner.",
+  },
+];
+
+export const backTechs = [
+  {
+    tech: "Python",
+    icon: <SiPython />,
+    text: "Why did the programmer quit using Python? Because he found it too constricting. 🐍😄",
+  },
+  {
+    tech: "Django",
+    icon: <SiDjango />,
+    text: "The web framework named after a guitarist, but won't leave you fretting over your code. 🎸💻😉",
+  },
+  {
+    tech: "GraphQL",
+    icon: <SiGraphql />,
+    text: "the query language that looks like a menu, but you still have to do the cooking. 🍽️💻",
+  },
+  {
+    tech: "NodeJS",
+    icon: <FaNodeJs />,
+    text: "The runtime that's like Tony Stark's suit - it may not be invincible, but it can handle almost anything you throw at it.",
+  },
+];
