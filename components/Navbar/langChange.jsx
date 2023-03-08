a49a66678473c2b28de49c458e8909f6541d8fa7
lordrichard6 <paulolopesreizinho@gmail.com>
2023-03-08 11:31:11 +0100

    chore: languange selected bold

--- components/Navbar/langChange.jsx
+++ components/Navbar/langChange.jsx
@@ -1,96 +1,33 @@
 import styled from "styled-components";
-import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import pt from "../../assets/images/pt.svg";
-import en from "../../assets/images/en.svg";
 
 export default function LangChange() {
   let router = useRouter();
 
-  console.log(router.locales);
+  console.log("languages", router.locales);
+  console.log("selected language", JSON.stringify(router.locale));
   return (
-    <NavMenu>
+    <NavMenu className="flex flex-row justify-center items-center list-none ">
       {router.locales.map((locale) => (
-        <NavItem key={locale}>
-          <Link
-            href={router.asPath}
-            locale={locale}
-            passHref
-          >
-            {/* 
-            USE FLAGS
-            <div className="flag">
-              {locale === "en" && <Image src={en} cover alt="" />}
-              {locale === "pt" && <Image src={pt} cover alt="" />}
-            </div> 
-            */}
-            <div className={router.locales === locale ? "selected" : ""}>
-            {locale === "pt" && <h3>pt</h3>}
-            {locale === "en" && <h3>en</h3>}
+        <li
+          className="text-2xl text-slate-100 font-normal uppercase m-0 px-1"
+          key={locale}
+        >
+          <Link href={router.asPath} locale={locale} passHref>
+            <div
+              className={
+                router.locale === locale ? "font-semibold" : "font-extralight"
+              }
+            >
+              {locale === "pt" && <h3>pt</h3>}
+              {locale === "en" && <h3>en</h3>}
             </div>
           </Link>
-        </NavItem>
+        </li>
       ))}
     </NavMenu>
   );
 }
 
-const NavMenu = styled.ul`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  list-style: none;
-  color: #fff;
-  font-size: 1.8rem;
-  font-weight: 400;
-  text-transform: uppercase;
-  margin: 0;
-  /* z-index: 2; */
-
-  @media screen and (max-width: 746px) {
-    font-size: 1.6rem;
-    padding-right: 1.5rem;
-  }
-`;
-
-const NavItem = styled.li`
-  padding: 0 0.3rem;
-
-  h3 {
-    margin: 0;
-    font-weight: 200;
-    &:hover {
-    font-weight: 600;
-  }
-  }
-  &.selected {
-    h3 {
-      font-weight: 4200;
-    }
-  }
-
-  /* FLAG STYLING
-  .flag {
-    display: flex;
-    width: 46px;
-    height: 32px;
-    border-radius: 10px;
-    &:hover {
-      border: 4px solid #fff;
-    }
-    span {
-      border-radius: 10px;
-    }
-  }
-  &.selected {
-    .flag {
-      border: 4px solid #fff;
-    }
-  }
-
-  &:hover {
-    font-weight: 800;
-  } */
-`;
+const NavMenu = styled.ul``;
