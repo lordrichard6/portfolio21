d37b59aef631f1c065993bfdb88426ceacef639e
lordrichard6 <paulolopesreizinho@gmail.com>
2023-03-08 13:16:14 +0100

    chore: small height fix

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -5,7 +5,7 @@ import Layout from "../components/Layout";
 export default function ProjectsPage() {
   return (
     <Layout>
-      <div className="h-screen flex flex-col justify-center items-center">
+      <div className="overflow-y-hidden h-screen flex flex-col justify-center items-center">
         <h1 className="text-slate-100 text-6xl mb-20">Contact Me 😉</h1>
         <ContactCard />
         <Contacts />
