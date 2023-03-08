2a393d352c407c88b94aa86162ff44e0297d8456
lordrichard6 <paulolopesreizinho@gmail.com>
2023-03-08 10:23:12 +0100

    chore:nav logo clean

--- components/Navbar/logo.jsx
+++ components/Navbar/logo.jsx
@@ -2,71 +2,26 @@ import styled from "styled-components";
 import Image from "next/image";
 import Link from "next/link";
 
-import { Colors } from "../../assets/variables";
 import logo from "../../public/logo_clean.png";
 
 export default function Logo() {
   return (
-    <LogoContainer>
+    <LogoContainer className="flex justify-center items-center">
       <div>
         <Link href="/" passHref>
-          <LogoWrapper
+          <Image
             src={logo}
             alt="paulo lopes reizinho"
             responsive
+            className="w-auto h-16 md:w-20 md:h-20 cursor-pointer"
           />
         </Link>
       </div>
-      <h1 className="fullname">Paulo Lopes Reizinho</h1>
-      <h1 className="partialname">Paulo Lopes R</h1>
+      <h1 className="w-10 tracking-tight leading-7 md:leading-6 text-2xl md:text-4xl font-normal text-slate-100">
+        Paulo Reizinho
+      </h1>
     </LogoContainer>
   );
 }
 
-const LogoContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  h1 {
-    color: ${Colors.white};
-    font-size: 2.2rem;
-    font-weight: 400;
-    letter-spacing: -1px;
-    margin: 0;
-
-    &.fullname {
-      @media screen and (max-width: 1200px) {
-        display: none;
-      }
-      @media screen and (max-width: 1024px) {
-        display: block;
-      }
-      @media screen and (max-width: 768px) {
-        display: none;
-      }
-    }
-    &.partialname {
-      @media screen and (min-width: 1200px) {
-        display: none;
-      }
-      @media screen and (max-width: 1200px) {
-        display: block;
-      }
-      @media screen and (max-width: 1024px) {
-        display: none;
-      }
-    }
-  }
-`;
-
-const LogoWrapper = styled(Image)`
-  width: 80px;
-  height: 80px;
-  margin-right: 0.5rem;
-  cursor: pointer;
-
-  @media screen and (max-width: 768px) {
-    width: 60px;
-    height: 60px;
-  }
-`;
+const LogoContainer = styled.div``;
