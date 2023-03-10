82d140c062114a70d76f4e3627db2d646028527f
lordrichard6 <paulolopesreizinho@gmail.com>
2023-03-10 14:13:54 +0100

    chore: unecessary stuff

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,4 @@
 import "../styles/main.scss";
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "../config/theme";
 import Router from "next/router";
 import NProgress from "nprogress"; //nprogress module
 
@@ -12,11 +10,7 @@ Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return (
-    // <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    // </ChakraProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
