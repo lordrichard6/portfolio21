fef842f5a76d1a51601a5670cfca03d0cc0fd8ca
lordrichard6 <paulolopesreizinho@gmail.com>
2023-03-15 18:18:37 +0100

    chore: contact page refactor

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,10 +1,46 @@
-import ContactsPage from "../components/_contact";
-import Layout from "../components/Layout";
+import React, { useEffect } from "react";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-export default function ProjectsPage() {
+import { bringUp } from "../utilities/framer-animations";
+
+import Layout from "../components/Layout";
+import {
+  ContactCard,
+  CVButton,
+  MapComponent,
+  ContactForm,
+} from "../components/_contact";
+
+export default function ContactPage() {
+
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    threshold: 0.2,
+  });
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+    // if (!inView) {
+    //   controls.start("hidden");
+    // }
+  }, [controls, inView]);
+
   return (
     <Layout>
-      <ContactsPage />
+      <motion.div
+        ref={ref}
+        variants={bringUp}
+        initial="hidden"
+        animate={controls}
+        className="overflow-hidden flex flex-col justify-center items-center pt-36 pb-10"
+      >
+        <ContactCard />
+        <CVButton />
+        <MapComponent />
+        <ContactForm />
+      </motion.div>
     </Layout>
   );
 }
