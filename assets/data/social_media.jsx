186cb08886812d473f4336291c9cf2ba053a5a65
lordrichard6 <paulolopesreizinho@gmail.com>
2023-04-28 09:42:23 +0200

    chore:added the forgoten links

--- assets/data/social_media.jsx
+++ assets/data/social_media.jsx
@@ -4,16 +4,16 @@ export const SocialMedia = [
     {
         icon: <SiGithub className='fa' />,
         iconName: "Github",
-        link: "/"
+        link: "https://github.com/lordrichard6"
     },
     {
         icon: <SiLinkedin className='fa' />,
         iconName: "Linkedin",
-        link: "/"
+        link: "https://www.linkedin.com/in/pauloreizinho/"
     },
     {
         icon: <SiInstagram className='fa' />,
         iconName: "Instagram",
-        link: "/"
+        link: "https://www.instagram.com/paulo_reizinho/"
     },
 ]
