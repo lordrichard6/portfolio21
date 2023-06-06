6380f07f66a240dda6170537bc25d7f34ec34fe3
lordrichard6 <paulolopesreizinho@gmail.com>
2023-06-06 09:03:05 +0200

    small tweak in the footer

--- components/Layout/Footer/copyright.jsx
+++ components/Layout/Footer/copyright.jsx
@@ -1,25 +1,15 @@
-import styled from "styled-components";
 import useTranslation from "next-translate/useTranslation";
 
 export default function CopyrightComponent() {
   let { t } = useTranslation();
 
   return (
-    <CopyrightWrapper className="section-x-default">
-      <p>Copyright &copy; {new Date().getFullYear()} Paulo Lopes Reizinho.</p>
-      <p>{t("common:rights_reserved")}</p>
-    </CopyrightWrapper>
+    <div className="section-x-default">
+      <hr className="opacity-20" />
+      <div className="flex justify-between text-sm">
+        <p>Copyright &copy; {new Date().getFullYear()} Paulo Lopes Reizinho.</p>
+        <p>{t("common:rights_reserved")}</p>
+      </div>
+    </div>
   );
 }
-
-const CopyrightWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  p {
-    margin-bottom: 0;
-
-    @media screen and (max-width: 764px) {
-      font-size: 0.8rem;
-    }
-  }
-`;
