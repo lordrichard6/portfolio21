3bbde6ed73357d8a37c204b50b1e4046a71ceae4
lordrichard6 <paulolopesreizinho@gmail.com>
2023-07-10 19:11:08 +0200

    improve project card design

--- components/_shared/projectCard.jsx
+++ components/_shared/projectCard.jsx
@@ -14,9 +14,11 @@ export default function ProjectCard({ imageSrc, cardHeading, cardText, cardId, i
         <div className={`cards-shared ${style}`}>
             <input type="checkbox" id={cardId} className="more" aria-hidden="true" />
             <div className="content">
-                <div className="front"
+                <div className="front relative"
                     style={{ backgroundImage: `url(${imageSrc})` }}
                 >
+                    <div className="front-square top absolute border-slate-100" />
+                    <div className="front-square bottom absolute border-slate-100" />
                     <div className="inner p-4 text-center">
                         <div className="tag-wrapper text-xl font-bold ml-auto text-slate-800">
                             {tags.map((tag, index) => <span className="mx-1" key={index}>{tag}</span>)}
@@ -27,21 +29,24 @@ export default function ProjectCard({ imageSrc, cardHeading, cardText, cardId, i
                         </label>
                     </div>
                 </div>
-                <div className="back bg-slate-900">
-                    <div className="inner p-2">
+                <div className="back bg-slate-900 relative">
+                    <div className="absolute w-1/2 h-full top-0 left-[30%] -skew-x-[45deg] backdrop-invert z-10"></div>
+                    <div className="inner p-4">
                         <div className="description">
                             <p>{t(cardText)}</p>
                         </div>
-                        <div className="name w-full flex justify-between">
+                        <div className="name w-full flex justify-between transs">
                             <span>{cardHeading}</span>
-                            <a href={github} target="_blank" rel="noopener noreferrer"><SiGithub /></a>
+                            {github && <a href={github} className="z-20 text-slate-900 text-4xl hover:translate-y-1 transition duration-150 ease-in-out" target="_blank" rel="noopener noreferrer"><SiGithub /></a>}
                         </div>
-                        <label for={cardId} className="button return" aria-hidden="true">
-                            {t(buttons.back)}
-                        </label>
-                        <a className="button return visit" href={link} target="_blank" rel="noopener noreferrer" aria-hidden="true">
-                            {t(buttons.visit)}
-                        </a>
+                        <div className="button-wrapper w-full flex justify-between z-20">
+                            <label for={cardId} className="button return " aria-hidden="true">
+                                {t(buttons.back)}
+                            </label>
+                            <a className="button return visit" href={link} target="_blank" rel="noopener noreferrer" aria-hidden="true">
+                                {t(buttons.visit)}
+                            </a>
+                        </div>
                     </div>
                 </div>
 
