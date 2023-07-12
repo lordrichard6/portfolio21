a0139a02d33076ad8029e8b7750c7c5cab3700e8
lordrichard6 <paulolopesreizinho@gmail.com>
2023-07-12 18:28:48 +0200

    some bugs fixed

--- components/_homepage/Header/image.jsx
+++ components/_homepage/Header/image.jsx
@@ -4,10 +4,10 @@ import SignatureComponent from "./signature";
 
 export default function MyImage() {
   return (
-    <div className="logo-container flex-centered w-full xl:w-1/2 relative z-10">
+    <div className="logo-container flex-centered w-full xl:w-1/2 z-10">
       <EntryAnimation style="w-full" animation={bringFromRight}>
         <ImagesComponent
-          style="image w-full p-0 xl:p-6 max-w-[700px] lg:mx-auto"
+          style="image w-full p-0 xl:p-6 max-h-[80vh] max-w-[700px] lg:mx-auto"
           alt="paulo reizinho crossed arms"
           src="/logo_crossed.png"
           priority="true"
