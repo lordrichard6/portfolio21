79dd74c24fcedbfc8da39258b7ee10a7619a2998
lordrichard6 <paulolopesreizinho@gmail.com>
2023-07-18 20:14:28 +0200

    style:improvments for landscape view in mobile

--- components/_homepage/Header/image.jsx
+++ components/_homepage/Header/image.jsx
@@ -6,9 +6,9 @@ import { home_heroImage } from "../../../assets/data";
 export default function MyImage() {
   return (
     <div className="logo-container flex-centered w-full xl:w-1/2 z-10">
-      <EntryAnimation style="w-full" animation={bringFromRight}>
+      <EntryAnimation style="w-full md:w-[75vw] lg:w-full max-h-[80vh] max-w-[700px]" animation={bringFromRight}>
         <ImagesComponent
-          style="image w-full p-0 xl:p-6 max-h-[80vh] max-w-[700px] lg:mx-auto"
+          style="image w-full p-0 xl:p-6 lg:mx-auto"
           alt={home_heroImage.alt}
           src={home_heroImage.img}
           priority="true"
--- components/_homepage/Header/signature.jsx
+++ components/_homepage/Header/signature.jsx
@@ -17,7 +17,7 @@ const svgVariants = {
 
 export default function SignatureComponent() {
   return (
-    <div className="signature z-20 absolute bottom-0 sm:bottom-[5vh] md:bottom-[10vh] lg:bottom-[20vh] xl:bottom-0 lg:top-[unset] max-w-[700px]">
+    <div className="signature z-20 absolute bottom-0 sm:bottom-[5vh] md:bottom-[10vh] lg:bottom-[20vh] xl:bottom-0 lg:top-[unset] md:max-w-[600px] lg:max-w-[700px]">
       <svg
         width="1548"
         height="552"
