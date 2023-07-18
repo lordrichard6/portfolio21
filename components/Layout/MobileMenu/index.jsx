184b9ffc8d25f7e64d1b7646cc3ff2374a9a0132
lordrichard6 <paulolopesreizinho@gmail.com>
2023-07-18 20:29:11 +0200

    style:tablet landscape fix bigger size

--- components/Layout/MobileMenu/index.jsx
+++ components/Layout/MobileMenu/index.jsx
@@ -14,10 +14,10 @@ export default function MobileMenu({ isOpen, toggle }) {
   return (
     <side isOpen={isOpen} onClick={toggle} className={`mobile-menu ${isOpen ? "right-[-60%] sm:right-[-80%]" : "right-[-100%]"} fixed top-0 right-0 w-full h-auto rounded-bl-xl flex flex-col z-40`}>
       <div onClick={toggle}>
-        <FaTimes className="text-5xl ml-[20%] sm:ml-[10%] mt-3" />
+        <FaTimes className="icon-close text-5xl ml-[20%] sm:ml-[10%] mt-3" />
       </div>
       <div>
-        <div className="flex flex-col justify-start items-start pl-4 mt-6">
+        <div className="list-wrapper flex flex-col justify-start items-start pl-4 mt-6">
           {navItems.map((item, i) => {
             return (
               <div className="mobile-item flex-centered" key={i} onClick={toggle}>
