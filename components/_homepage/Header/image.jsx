b0e06cdb20b82c8fc1b094859205b9a8e1193b61
lordrichard6 <paulolopesreizinho@gmail.com>
2023-10-10 12:09:48 +0200

    fix:homepage header responsive fixes

--- components/_homepage/Header/image.jsx
+++ components/_homepage/Header/image.jsx
@@ -6,7 +6,7 @@ import { home_heroImage } from "../../../assets/data";
 export default function MyImage() {
   return (
     <div className="logo-container flex-centered w-full xl:w-1/2 z-10">
-      <EntryAnimation style="w-full md:w-[75vw] lg:w-full max-h-[80vh] max-w-[700px]" animation={bringFromRight}>
+      <EntryAnimation style="px-4 w-full md:px-0 md:w-[75vw] lg:w-full max-h-[80vh] max-w-[700px]" animation={bringFromRight}>
         <ImagesComponent
           style="image w-full p-0 xl:p-6 lg:mx-auto"
           alt={home_heroImage.alt}
